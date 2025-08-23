ef9b89e106ddfc3084540f9f123972a9530f61a2
servesh21 <serveshkhade68@gmail.com>
2025-08-24 00:28:30 +0530

    migrated auth to use cookies

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -50,8 +50,8 @@ export class AuthController {
     const isProduction = process.env.NODE_ENV === 'production';
     res.cookie('jwt_token', jwtToken, {
       httpOnly: true,
-      sameSite: 'lax',
-      secure: isProduction, // Only secure in production
+      sameSite: 'none',
+      secure: true, // Only secure in production
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
